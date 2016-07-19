3e55d63d88b4c17d53b5093f131aad3e216a7790
Kirill Tikhonov <slimline@inbox.ru>
2016-07-19 19:39:42 +0300

    Enums now enum classes

--- PulseGenerator.h
+++ PulseGenerator.h
@@ -9,8 +9,8 @@
 //Class for generating impulses by timer.
 //use case
 //
-//PulseGenerator<TIM4> pulseGeneratorTim4(); //Default constructor sets positive polarity
-//PulseGenerator<TIM2> pulseGeneratorTim2(PulseGeneratorPolarity_Negative);//You may set polarity in constructor
+//PulseGenerator<PulseGeneratorTimers::Tim4> pulseGeneratorTim4(); //Default constructor sets positive polarity
+//PulseGenerator<PulseGeneratorTimers::Tim2> pulseGeneratorTim2(PulseGeneratorPolarity::Negative);//You may set polarity in constructor
 //pulseGeneratorTim2.init();
 //pulseGeneratorTim4.init();
 //pulseGeneratorTim4.GeneratePulseASync(GPIOA,10,25); Wait  timer 4 availability, generates impulse, don't wait end of impulse  (On GPIOA port, 10 pin, for 25 microseconds)
@@ -36,31 +36,31 @@
 //******************************************************************************************
 //Public types
 //******************************************************************************************
-enum PulseGeneratorPolarity_t
+enum class PulseGeneratorPolarity
 {
-	PulseGeneratorPolarity_Positive,
-	PulseGeneratorPolarity_Negative
+	Positive,
+	Negative
 };
-enum PulseGeneratorTimers_t
+enum class PulseGeneratorTimers
 {
-	PulseGeneratorTIM1,
-	PulseGeneratorTIM2,
-	PulseGeneratorTIM3,
-	PulseGeneratorTIM4,
-	PulseGeneratorTIM5,
-	PulseGeneratorTIM6,
-	PulseGeneratorTIM7,
-	PulseGeneratorTIM8,
-	PulseGeneratorTIM9,
-	PulseGeneratorTIM10,
-	PulseGeneratorTIM11,
-	PulseGeneratorTIM12,
-	PulseGeneratorTIM13,
-	PulseGeneratorTIM14
+	Tim1,
+	Tim2,
+	Tim3,
+	Tim4,
+	Tim5,
+	Tim6,
+	Tim7,
+	Tim8,
+	Tim9,
+	Tim10,
+	Tim11,
+	Tim12,
+	Tim13,
+	Tim14
 };
 
 
-template <PulseGeneratorTimers_t TIM>
+template <PulseGeneratorTimers TIM>
 class PulseGenerator
 {
 public:
@@ -103,33 +103,32 @@ private:
 //******************************************************************************************
 //Template definitions of static variables
 //******************************************************************************************
-template <PulseGeneratorTimers_t TIM>
+template <PulseGeneratorTimers TIM>
 volatile  std::atomic<bool>  PulseGenerator<TIM>::isBusy(0);
 
-template <PulseGeneratorTimers_t TIM>
+template <PulseGeneratorTimers TIM>
 bool PulseGenerator<TIM>::isInited=0;
 
-template <PulseGeneratorTimers_t TIM>
+template <PulseGeneratorTimers TIM>
 GPIO_TypeDef * PulseGenerator<TIM>::currentPort=NULL;
 
-template <PulseGeneratorTimers_t TIM>
+template <PulseGeneratorTimers TIM>
 uint16_t PulseGenerator<TIM>::currentPin=0;
 
-template <PulseGeneratorTimers_t TIM>
+template <PulseGeneratorTimers TIM>
 bool PulseGenerator<TIM>::currentPolarity=0;
 
-template <PulseGeneratorTimers_t TIM>
+template <PulseGeneratorTimers TIM>
 uint32_t PulseGenerator<TIM>::freq=DEFAULT_SYS_FREQ;
 
 //******************************************************************************************
 //Template definitions of public functions
 //******************************************************************************************
-template <PulseGeneratorTimers_t TIM>
+template <PulseGeneratorTimers TIM>
 void PulseGenerator<TIM>::init()
 {
 	if (!isInited)
 	{
-
 		timCLockEnable();
 		initTimer();
 		initInterrupt();
@@ -137,7 +136,7 @@ void PulseGenerator<TIM>::init()
 	}
 }
 
-template <PulseGeneratorTimers_t TIM>
+template <PulseGeneratorTimers TIM>
 void PulseGenerator<TIM>::initTimer()
 {
 	constexpr uint32_t RepetitionCounter = 0;
@@ -170,17 +169,17 @@ void PulseGenerator<TIM>::initTimer()
 }
 
 
-template <PulseGeneratorTimers_t TIM>
+template <PulseGeneratorTimers TIM>
 void PulseGenerator<TIM>::generatePulseAsync(GPIO_TypeDef *Port,uint16_t  Pin,uint32_t widthMs){while (tryGeneratePulse(Port,Pin,widthMs));}
 
-template <PulseGeneratorTimers_t TIM>
+template <PulseGeneratorTimers TIM>
 void PulseGenerator<TIM>::generatePulseSync(GPIO_TypeDef *Port,uint16_t  Pin,uint32_t widthMs)
 {
 	generatePulseAsync(Port,Pin,widthMs);
 	while(isBusy);
 }
 
-template <PulseGeneratorTimers_t TIM>
+template <PulseGeneratorTimers TIM>
 bool PulseGenerator<TIM>::tryGeneratePulse(GPIO_TypeDef *Port,uint16_t  Pin, int32_t widthMs)
 {
 	bool freeState=0;
@@ -199,7 +198,7 @@ bool PulseGenerator<TIM>::tryGeneratePulse(GPIO_TypeDef *Port,uint16_t  Pin, int
 		return 1;
 }
 
-template <PulseGeneratorTimers_t TIM>
+template <PulseGeneratorTimers TIM>
 volatile void PulseGenerator<TIM>::irqHandler()
 {
 	getTimBase()->SR&=~TIM_FLAG_UPDATE;
@@ -210,76 +209,76 @@ volatile void PulseGenerator<TIM>::irqHandler()
 //******************************************************************************************
 //Template definitions of private functions
 //******************************************************************************************
-template <PulseGeneratorTimers_t TIM>
+template <PulseGeneratorTimers TIM>
 constexpr TIM_TypeDef * PulseGenerator<TIM>::getTimBase()
 {
 	switch (TIM)
 	{
-	case (PulseGeneratorTIM1):return TIM1;
-	case (PulseGeneratorTIM2):return TIM2;
-	case (PulseGeneratorTIM3):return TIM3;
-	case (PulseGeneratorTIM4):return TIM4;
-	case (PulseGeneratorTIM5):return TIM5;
-	case (PulseGeneratorTIM6):return TIM6;
-	case (PulseGeneratorTIM7):return TIM7;
-	case (PulseGeneratorTIM8):return TIM8;
-	case (PulseGeneratorTIM9):return TIM9;
-	case (PulseGeneratorTIM10):return TIM10;
-	case (PulseGeneratorTIM11):return TIM11;
-	case (PulseGeneratorTIM12):return TIM12;
-	case (PulseGeneratorTIM13):return TIM13;
-	case (PulseGeneratorTIM14):return TIM14;
+	case (PulseGeneratorTimers::Tim1):return TIM1;
+	case (PulseGeneratorTimers::Tim2):return TIM2;
+	case (PulseGeneratorTimers::Tim3):return TIM3;
+	case (PulseGeneratorTimers::Tim4):return TIM4;
+	case (PulseGeneratorTimers::Tim5):return TIM5;
+	case (PulseGeneratorTimers::Tim6):return TIM6;
+	case (PulseGeneratorTimers::Tim7):return TIM7;
+	case (PulseGeneratorTimers::Tim8):return TIM8;
+	case (PulseGeneratorTimers::Tim9):return TIM9;
+	case (PulseGeneratorTimers::Tim10):return TIM10;
+	case (PulseGeneratorTimers::Tim11):return TIM11;
+	case (PulseGeneratorTimers::Tim12):return TIM12;
+	case (PulseGeneratorTimers::Tim13):return TIM13;
+	case (PulseGeneratorTimers::Tim14):return TIM14;
 	default:while(1);
 	}
 }
 
-template <PulseGeneratorTimers_t TIM>
+template <PulseGeneratorTimers TIM>
 void PulseGenerator<TIM>::timCLockEnable() {
 	switch (TIM)
 	{
-	case(PulseGeneratorTIM1):{	RCC->APB2ENR|=RCC_APB2ENR_TIM1EN;break;	}
-	case(PulseGeneratorTIM2):{	RCC->APB1ENR|=RCC_APB1ENR_TIM2EN;break;	}
-	case(PulseGeneratorTIM3):{	RCC->APB1ENR|=RCC_APB1ENR_TIM3EN;break;	}
-	case(PulseGeneratorTIM4):{	RCC->APB1ENR|=RCC_APB1ENR_TIM4EN;break;	}
-	case(PulseGeneratorTIM5):{	RCC->APB1ENR|=RCC_APB1ENR_TIM5EN;break;	}
-	case(PulseGeneratorTIM6):{	RCC->APB1ENR|=RCC_APB1ENR_TIM6EN;break;	}
-	case(PulseGeneratorTIM7):{	RCC->APB1ENR|=RCC_APB1ENR_TIM7EN;break;	}
-	case(PulseGeneratorTIM8):{	RCC->APB2ENR|=RCC_APB2ENR_TIM8EN;break;	}
-	case(PulseGeneratorTIM9):{	RCC->APB2ENR|=RCC_APB2ENR_TIM9EN;break;	}
-	case(PulseGeneratorTIM10):{	RCC->APB2ENR|=RCC_APB2ENR_TIM10EN;break;}
-	case(PulseGeneratorTIM11):{	RCC->APB2ENR|=RCC_APB2ENR_TIM11EN;break;}
-	case(PulseGeneratorTIM12):{	RCC->APB1ENR|=RCC_APB1ENR_TIM12EN;break;}
-	case(PulseGeneratorTIM13):{	RCC->APB1ENR|=RCC_APB1ENR_TIM13EN;break;}
-	case(PulseGeneratorTIM14):{	RCC->APB1ENR|=RCC_APB1ENR_TIM14EN;break;}
+	case(PulseGeneratorTimers::Tim1):{	RCC->APB2ENR|=RCC_APB2ENR_TIM1EN;break;	}
+	case(PulseGeneratorTimers::Tim2):{	RCC->APB1ENR|=RCC_APB1ENR_TIM2EN;break;	}
+	case(PulseGeneratorTimers::Tim3):{	RCC->APB1ENR|=RCC_APB1ENR_TIM3EN;break;	}
+	case(PulseGeneratorTimers::Tim4):{	RCC->APB1ENR|=RCC_APB1ENR_TIM4EN;break;	}
+	case(PulseGeneratorTimers::Tim5):{	RCC->APB1ENR|=RCC_APB1ENR_TIM5EN;break;	}
+	case(PulseGeneratorTimers::Tim6):{	RCC->APB1ENR|=RCC_APB1ENR_TIM6EN;break;	}
+	case(PulseGeneratorTimers::Tim7):{	RCC->APB1ENR|=RCC_APB1ENR_TIM7EN;break;	}
+	case(PulseGeneratorTimers::Tim8):{	RCC->APB2ENR|=RCC_APB2ENR_TIM8EN;break;	}
+	case(PulseGeneratorTimers::Tim9):{	RCC->APB2ENR|=RCC_APB2ENR_TIM9EN;break;	}
+	case(PulseGeneratorTimers::Tim10):{	RCC->APB2ENR|=RCC_APB2ENR_TIM10EN;break;}
+	case(PulseGeneratorTimers::Tim11):{	RCC->APB2ENR|=RCC_APB2ENR_TIM11EN;break;}
+	case(PulseGeneratorTimers::Tim12):{	RCC->APB1ENR|=RCC_APB1ENR_TIM12EN;break;}
+	case(PulseGeneratorTimers::Tim13):{	RCC->APB1ENR|=RCC_APB1ENR_TIM13EN;break;}
+	case(PulseGeneratorTimers::Tim14):{	RCC->APB1ENR|=RCC_APB1ENR_TIM14EN;break;}
 	}
 }
-template <PulseGeneratorTimers_t TIM>
+template <PulseGeneratorTimers TIM>
 void PulseGenerator<TIM>::enableTimer()
 {
 	getTimBase()->CNT=0;
 	getTimBase()->CR1|=TIM_CR1_CEN;
 };
 
-template <PulseGeneratorTimers_t TIM>
+template <PulseGeneratorTimers TIM>
 void PulseGenerator<TIM>::initInterrupt()
 {
 	IRQn_Type irqNumber;
 	switch (TIM)
 	{
-	case(PulseGeneratorTIM1):{irqNumber=TIM1_UP_TIM10_IRQn;break;		}
-	case(PulseGeneratorTIM2):{irqNumber=TIM2_IRQn;break;				}
-	case(PulseGeneratorTIM3):{irqNumber=TIM3_IRQn;break;				}
-	case(PulseGeneratorTIM4):{irqNumber=TIM4_IRQn;break;				}
-	case(PulseGeneratorTIM5):{irqNumber=TIM5_IRQn;break;				}
-	case(PulseGeneratorTIM6):{irqNumber=TIM6_DAC_IRQn;break;			}
-	case(PulseGeneratorTIM7):{irqNumber=TIM7_IRQn;break;				}
-	case(PulseGeneratorTIM8):{irqNumber=TIM8_UP_TIM13_IRQn;break;		}
-	case(PulseGeneratorTIM9):{irqNumber=TIM1_BRK_TIM9_IRQn;break;		}
-	case(PulseGeneratorTIM10):{irqNumber=TIM1_UP_TIM10_IRQn;break;		}
-	case(PulseGeneratorTIM11):{irqNumber=TIM1_TRG_COM_TIM11_IRQn;break;	}
-	case(PulseGeneratorTIM12):{irqNumber=TIM8_BRK_TIM12_IRQn;break;		}
-	case(PulseGeneratorTIM13):{irqNumber=TIM8_UP_TIM13_IRQn;break;		}
-	case(PulseGeneratorTIM14):{irqNumber=TIM8_TRG_COM_TIM14_IRQn;break;	}
+	case(PulseGeneratorTimers::Tim1):{irqNumber=TIM1_UP_TIM10_IRQn;break;		}
+	case(PulseGeneratorTimers::Tim2):{irqNumber=TIM2_IRQn;break;				}
+	case(PulseGeneratorTimers::Tim3):{irqNumber=TIM3_IRQn;break;				}
+	case(PulseGeneratorTimers::Tim4):{irqNumber=TIM4_IRQn;break;				}
+	case(PulseGeneratorTimers::Tim5):{irqNumber=TIM5_IRQn;break;				}
+	case(PulseGeneratorTimers::Tim6):{irqNumber=TIM6_DAC_IRQn;break;			}
+	case(PulseGeneratorTimers::Tim7):{irqNumber=TIM7_IRQn;break;				}
+	case(PulseGeneratorTimers::Tim8):{irqNumber=TIM8_UP_TIM13_IRQn;break;		}
+	case(PulseGeneratorTimers::Tim9):{irqNumber=TIM1_BRK_TIM9_IRQn;break;		}
+	case(PulseGeneratorTimers::Tim10):{irqNumber=TIM1_UP_TIM10_IRQn;break;		}
+	case(PulseGeneratorTimers::Tim11):{irqNumber=TIM1_TRG_COM_TIM11_IRQn;break;	}
+	case(PulseGeneratorTimers::Tim12):{irqNumber=TIM8_BRK_TIM12_IRQn;break;		}
+	case(PulseGeneratorTimers::Tim13):{irqNumber=TIM8_UP_TIM13_IRQn;break;		}
+	case(PulseGeneratorTimers::Tim14):{irqNumber=TIM8_TRG_COM_TIM14_IRQn;break;	}
 	}
 	NVIC_SetPriority(irqNumber, DEFAULT_TIM_NVIC_PRIORITY);
 	NVIC_EnableIRQ (irqNumber);
