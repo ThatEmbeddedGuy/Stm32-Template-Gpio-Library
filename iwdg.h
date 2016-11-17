20edb6f757bcd8f10d19cfbfe829650499429c4a
Tikhonov Kirill <slimline@inbox.ru>
2016-11-17 18:00:06 +0300

    added watchdog class

---
+++ iwdg.h
@@ -0,0 +1,30 @@
+/*
+ * iwdg.h
+ *
+ *  Created on: 7 но€б. 2016 г.
+ *      Author: tihonov
+ */
+
+#ifndef INTERRUPT_IWDG_H_
+#define INTERRUPT_IWDG_H_
+#include "cmsis_device.h"
+
+class iwdg
+{
+public:
+	iwdg()=default;
+static void init(uint32_t us)
+{
+	IWDG->KR = 0x5555;
+	IWDG->PR = 3;   // Prescaler /32  (1 ms)
+	IWDG->RLR = us;        
+	IWDG->KR = 0xCCCC;        // Start
+};
+static void reset()
+{
+ IWDG->KR =  0xAAAA; };
+};
+
+
+
+#endif /* INTERRUPT_IWDG_H_ */
